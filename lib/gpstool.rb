9c3cbae89c69d2973cce6498a0f9cbb38173e48c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-05-22 02:01:54 +1200

    Initial commit including readme and license file.

---
+++ lib/gpstool.rb
@@ -0,0 +1,8 @@
+# This file is part of the "GPSTool" project, and is distributed under the MIT License.
+# Copyright (c) 2011 Samuel G. D. Williams. <http://www.oriontransfer.co.nz>
+# See <LICENSE.txt> for licensing details.
+
+module GPSTool
+	
+end
+
---
+++ lib/gpstool/version.rb
@@ -0,0 +1,13 @@
+# This file is part of the "GPSTool" project, and is distributed under the MIT License.
+# Copyright (c) 2011 Samuel G. D. Williams. <http://www.oriontransfer.co.nz>
+# See <LICENSE.txt> for licensing details.
+
+module GPSTool
+  module VERSION #:nodoc:
+    MAJOR = 0
+    MINOR = 1
+    TINY  = 0
+
+    STRING = [MAJOR, MINOR, TINY].join('.')
+  end
+end
